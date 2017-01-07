41af6a529b7146676e6d032b57889be24cb20e7a
justi <justi@192.168.0.102>
2017-01-07 14:45:12 +0800

    添加登录交互效果、登录的JSON数据、添加h.js和util.js

--- gm/js/user/login.js
+++ gm/js/user/login.js
@@ -1,8 +1,38 @@
+/**
+ * 复选框改变事件
+ */
+mui(document.body).on('change', 'input[type=checkbox]', function() {
+	var value = this.checked?"true":"false";
+	h(this).val(value);
+});
+
+/**
+ * 登录点击事件
+ */
 mui(document.body).on('tap', '.mui-btn', function(e) {
-    mui(this).button('loading');
+    var username = trimStr(h("#username").val());
+    var password = trimStr(h("#password").val());
+    var agreement = h("#agreement").val();
+    var _this = this;
     
-    
-    /*setTimeout(function() {
-        mui(this).button('reset');
-    }.bind(this), 2000);*/
+    if(agreement === "false"){
+    	mui.toast('您未同意违约标准约束');
+    }else if(username==""){
+    	mui.toast("用户名不能为空");
+    }else if(password==""){
+    	mui.toast("密码不能为空");
+    }else{
+    	mui(this).button('loading');
+    	
+    	//请求后台
+    	mui.getJSON(ipAdd+"/datas/login.json",{},function(data){
+  			if(data.username === username && data.password==password){
+  				mui.toast("登录成功");
+  			}else{
+  				mui.toast("用户名或密码错误");
+  			}
+  			
+  			mui(_this).button('reset');
+    	});    	
+    }
 });
