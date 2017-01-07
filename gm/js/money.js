a3b9aff9ccf902748897bea814cefdb029e36988
justi <justi@192.168.0.102>
2017-01-07 17:21:39 +0800

    新增资金页面、订单页面

---
+++ gm/js/money.js
@@ -0,0 +1,51 @@
+mui.ready(function(){
+	
+	// 基于准备好的dom，初始化echarts实例
+	var myChart = echarts.init(document.getElementById('main'));
+	
+	// 指定图表的配置项和数据
+	option = {
+	    tooltip: {
+	        trigger: 'item',
+	        formatter: "{a} <br/>{b}: {c} ({d}%)"
+	    },
+	    legend: {
+	        orient: 'vertical',
+	        x: 'left',
+	        data:['可提现','结算中']
+	    },
+	    series: [
+	        {
+	            name:'资金',
+	            type:'pie',
+	            radius: ['50%', '70%'],
+	            avoidLabelOverlap: false,
+	            label: {
+	                normal: {
+	                    show: false,
+	                    position: 'center'
+	                },
+	                emphasis: {
+	                    show: true,
+	                    textStyle: {
+	                        fontSize: '30',
+	                        fontWeight: 'bold'
+	                    }
+	                }
+	            },
+	            labelLine: {
+	                normal: {
+	                    show: false
+	                }
+	            },
+	            data:[
+	                {value:335, name:'可提现'},
+	                {value:1548, name:'结算中'}
+	            ]
+	        }
+	    ]
+	};
+	
+	// 使用刚指定的配置项和数据显示图表。
+	myChart.setOption(option);
+});
